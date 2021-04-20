816aec61d5a4a9857497ab88a81380b9d855011b
HOLEHUY <holehuy2003@gmail.com>
2021-04-20 10:37:51 +0700

    data

--- data/Data.js
+++ data/Data.js
@@ -3,15 +3,15 @@ import {images} from '../constants';
 export const dummyImageData = [
   {
     id: 1,
-    img:images.banner1
+    img: images.banner1,
   },
   {
     id: 2,
-    img:images.banner2
+    img: images.banner2,
   },
   {
     id: 3,
-    img:images.banner3
+    img: images.banner3,
   },
 ];
 export const dummyProjectData = [
@@ -20,8 +20,7 @@ export const dummyProjectData = [
     status: '1',
     img: images.project1,
     projectName: 'Đà Nẵng PEARL',
-    projectAddress:
-      'Đường Trần Đại Nghĩa, Hoà Hải, Ngũ Hành Sơn, Đà Nẵng',
+    projectAddress: 'Đường Trần Đại Nghĩa, Hoà Hải, Ngũ Hành Sơn, Đà Nẵng',
   },
   {
     id: 1,
@@ -149,7 +148,8 @@ export const dummyRentData = [
     img: images.rent3,
     title:
       'Căn hộ dịch vụ full tiện nghi, đối diện chợ Bà Chiểu, ưu đãi lớn dành cho sinh viên',
-    address: 'Phố Nguyễn Thiện Thuật - Phường 24, Quận Bình Thạnh, TP Hồ Chí Minh',
+    address:
+      'Phố Nguyễn Thiện Thuật - Phường 24, Quận Bình Thạnh, TP Hồ Chí Minh',
     price: '4,5tr/tháng',
     acreage: '28m2',
   },
@@ -158,7 +158,8 @@ export const dummyRentData = [
     img: images.rent4,
     title:
       'Cho thuê căn hộ dịch vụ cao cấp đường Nguyễn Cửu Vân - Quận Bình Thạnh',
-    address: 'Đường Nguyễn Cửu Vân - Phường 17, Quận Bình Thạnh, TP Hồ Chí Minh',
+    address:
+      'Đường Nguyễn Cửu Vân - Phường 17, Quận Bình Thạnh, TP Hồ Chí Minh',
     price: '7,9tr/tháng',
     acreage: '47m2',
   },
@@ -180,4 +181,34 @@ export const dummyRentData = [
     price: '8tr/tháng',
     acreage: '45m2',
   },
+];
+export const markers = [
+  {latlng: {latitude: 16.064090209868745, longitude: 108.14965546131134}},
+  {latlng: {latitude: 16.063813453170958, longitude: 108.15124366432428}},
+  {latlng: {latitude: 16.06350963251116, longitude: 108.15051879733801}},
+  {latlng: {latitude: 16.066487248281064, longitude: 108.15012082457542}},
+  {latlng: {latitude: 16.0655268254707, longitude: 108.15178412944078}},
+  {latlng: {latitude: 16.065264246807068, longitude: 108.15033439546825}},
+  {latlng: {latitude: 16.067934478458213, longitude: 108.15166343003511}},
+  {latlng: {latitude: 16.06796186361162, longitude: 108.15066128969193}},
+  {latlng: {latitude: 16.066804273779642, longitude: 108.15230648964642}},
+  {latlng: {latitude: 16.07008242957103, longitude: 108.15122287720443}},
+  {latlng: {latitude: 16.071238067287727, longitude: 108.14750399440527}},
+  {latlng: {latitude: 16.071412362775444, longitude: 108.14654778689146}},
+  {latlng: {latitude: 16.070862735450845, longitude: 108.14666848629713}},
+  {latlng: {latitude: 16.070203567265917, longitude: 108.14657259732485}},
+  {latlng: {latitude: 16.06375159364687, longitude: 108.14423337578773}},
+  {latlng: {latitude: 16.063242862768842, longitude: 108.14322017133236}},
+  {latlng: {latitude: 16.061993099757206, longitude: 108.1436325609684}},
+  {latlng: {latitude: 16.063242862768842, longitude: 108.1454236060381}},
+  {latlng: {latitude: 16.059896289737022, longitude: 108.14410865306854}},
+  {latlng: {latitude: 16.058088143874684, longitude: 108.14865097403525}},
+  {latlng: {latitude: 16.056667584401524, longitude: 108.14823891967534}},
+  {latlng: {latitude: 16.05703682129314, longitude: 108.14732193946838}},
+  {latlng: {latitude: 16.055230260450955, longitude: 108.14740743488073}},
+  {latlng: {latitude: 16.056008691388236, longitude: 108.1496923416853}},
+  {latlng: {latitude: 16.048296742349695, longitude: 108.15083730965853}},
+  {latlng: {latitude: 16.048125970931906, longitude: 108.14895037561655}},
+  {latlng: {latitude: 16.049283669392143, longitude: 108.14859498292208}},
+  {latlng: {latitude: 16.048177202372614, longitude: 108.14811520278454}},
 ];
--- screens/Map.js
+++ screens/Map.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Text, TouchableOpacity, View, Image} from 'react-native';
-import MapView, {Circle, Marker,Polygon} from 'react-native-maps';
+import MapView, {Circle, Marker, Polygon} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 
 class Map extends Component {
@@ -10,9 +10,15 @@ class Map extends Component {
       latitude: 0,
       longitude: 0,
       error: null,
+      markers: [],
     };
   }
-
+  onMapPress(e) {
+    this.setState({
+      markers: [...this.state.markers, {latlng: e.nativeEvent.coordinate}],
+    },()=>{console.log(this.state.markers)});
+    
+  }
   componentDidMount() {
     Geolocation.getCurrentPosition(
       (position) => {
@@ -40,9 +46,14 @@ class Map extends Component {
             latitudeDelta: 0.00922,
             longitudeDelta: 0.00421,
           }}
-        >
+          onPress={this.onMapPress.bind(this)}>
           <Marker coordinate={this.state} />
-          <Circle center={this.state} radius={200} fillColor="rgba(255,106,106,0.2)"/>
+          <Circle
+            center={this.state}
+            radius={200}
+            fillColor="rgba(255,106,106,0.2)"
+          />
+          {this.state.markers.map((marker,i)=>(<MapView.Marker key={i} coordinate={marker.latlng}/>))}
         </MapView>
       </View>
     );
